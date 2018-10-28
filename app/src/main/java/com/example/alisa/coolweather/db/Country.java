3f3bbf4246fd9213edd50f7022d734ddb1665c5b
dp-xp <2868714995@qq.com>
2018-10-28 15:00:36 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/alisa/coolweather/db/Country.java
@@ -0,0 +1,42 @@
+package com.example.alisa.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Country extends DataSupport {
+    private int id;
+    private String countryName;
+    private int countryCode;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(int countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
